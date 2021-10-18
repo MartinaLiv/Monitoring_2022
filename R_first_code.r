048c695d7cf0bb623dcc92ee60d7e65fab2c83d5
MartinaLiv <92572883+MartinaLiv@users.noreply.github.com>
2021-10-18 10:59:58 +0200

    Update R_first_code.r

--- R_first_code.r
+++ R_first_code.r
@@ -10,3 +10,8 @@ fishes <- c(10, 50, 60, 100, 200)
 
 # plot diversity of fishes (y) versus the amount of water (x)
 plot(water, fishes)
+
+# the data we developed can be stored in a table
+# a table in R is called data frame
+
+streams <- data.frame(water, fishes)
