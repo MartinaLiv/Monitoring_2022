7e4e019c6bd970baef29b5950ad5251ca93feca4
MartinaLiv <92572883+MartinaLiv@users.noreply.github.com>
2021-10-29 11:02:31 +0200

    Update R_code_remote_sensing.r

--- R_code_remote_sensing.r
+++ R_code_remote_sensing.r
@@ -6,3 +6,18 @@
 install.packages("raster")
 
 library(raster)
+
+setwd("/Users/name/lab/")
+
+# we are going to import satellite data
+l2011 <- brick("p224r63_2011.grd")
+
+plot(l2011)
+
+#change colour palette
+
+cl <- colorRampPalette(c("black","gray","light gray"))(100) 
+
+plot(l2011, col=cl)
+
+plotRGB(l2011, r=3, g=2, b=1, stretch="Lin")
