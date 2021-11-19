8d9d1e0c7acb2a9a7c39f05b50ed33c5df3d7fc3
MartinaLiv <92572883+MartinaLiv@users.noreply.github.com>
2021-11-19 11:46:53 +0100

    Update R_code_energy_rs.r

--- R_code_energy_rs.r
+++ R_code_energy_rs.r
@@ -14,7 +14,7 @@ l1992
 
 # Bands:  defor1_.1, defor1_.2, defor1_.3 
 #plotRGB
-plotRGB(L1992, r=1 , g=2 , b=3, stretch="Lin")
+plotRGB(l1992, r=1 , g=2 , b=3, stretch="Lin")
 
 #defor1_.1= NIR (only thing we know for sure because all vegetation reflects red)
 #defor1_.2= red
