7c8fad1c7154267891dbd73c160f26576bb35088
MartinaLiv <92572883+MartinaLiv@users.noreply.github.com>
2021-11-19 11:46:16 +0100

    Update R_code_energy_rs.r

--- R_code_energy_rs.r
+++ R_code_energy_rs.r
@@ -3,6 +3,8 @@
 #recall library raster
 library(raster)
 
+#install.package "rgdal"
+
 #set the working directory
 setwd("C:/Users/marti/OneDrive/Documents/lab")
 
@@ -13,3 +15,8 @@ l1992
 # Bands:  defor1_.1, defor1_.2, defor1_.3 
 #plotRGB
 plotRGB(L1992, r=1 , g=2 , b=3, stretch="Lin")
+
+#defor1_.1= NIR (only thing we know for sure because all vegetation reflects red)
+#defor1_.2= red
+#defor1_.3= green
+
