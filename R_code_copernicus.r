04b2fe29435424561c95240776fa7ab274ba2152
MartinaLiv <92572883+MartinaLiv@users.noreply.github.com>
2021-12-17 10:25:46 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -39,3 +39,53 @@ geom_raster(snow20211214, mapping = aes(x = x, y = y, fill = Snow.Cover.Extent))
 scale_fill_viridis(option="cividis") +
 ggtitle("cividis palette")
 
+#---day2---
+#set wd and recall libraries
+#lapply function apply a function to a set of objects
+rlist <- list.files(pattern = "SCE")#create a list with all the objects with the pattern chosen
+list_rast <- lapply(rlist, raster)#lapply function
+snow_stack <- stack(list_rast)#make a stack
+
+#rename objects
+ssummer<- snow_stack$Snow.Cover.Extent.1
+swinter<- snow_stack$Snow.Cover.Extent.2 
+
+#ggplot
+p1 <- ggplot()+
+geom_raster(ssummer, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.1))+
+scale_fill_viridis(option= "viridis")+
+ggtitle("Snow cover during summer")
+
+p2 <- ggplot()+
+geom_raster(swinter, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.2))+
+scale_fill_viridis(option= "viridis")+
+ggtitle("Snow cover during winter")
+
+#patchwork them together
+#library patchwork
+p1/p2 #one image on top of the other
+
+#how to crop the image on a certain area-> coordinates
+#we want to zoom on italy
+#longitude from 0 to 20
+#latitude from 30 to 50
+ext <- c(0, 20, 30, 50)
+ssummer_cropped <- crop(ssummer, ext)
+swinter_cropped <- crop(swinter, ext)
+#stack_cropped <- crop(snow_stack, ext)
+
+#ggplot cropped
+p1 <- ggplot()+
+geom_raster(ssummer_cropped, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.1))+
+scale_fill_viridis(option= "viridis")+
+ggtitle("Snow cover during summer")
+
+p2 <- ggplot()+
+geom_raster(swinter_cropped, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.2))+
+scale_fill_viridis(option= "viridis")+
+ggtitle("Snow cover during winter")
+
+p1/p2
+                 
+
+
